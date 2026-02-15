72ed579608c016f581c542a8da3e4d7500ee482f
AtlasElys <atlaselys11@gmail.com>
2026-02-15 22:54:08 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TOKEN = os.getenv('TOKEN')
-ADMIN_IDS = list(map(int, os.getenv('ADMIN_IDS', '').split(','))) if os.getenv('ADMIN_IDS') else []
-DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///data/bot_database.db')
